69cf92e97767795fdf134eef1a2118aeff451eb2
Griko Nibras <git@griko.id>
2021-05-02 05:11:20 +0700

    refactor: update config, deps, etc. for v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,26 @@
 module.exports = {
   extends: [
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/react",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "import", "react"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'react'],
+
   rules: {
-    //
+    // add your own!
   },
   settings: {
-    "import/extensions": [".js", ".jsx", ".mjs", ".ts", ".tsx"],
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
-    },
-    "import/resolver": {
-      node: {
-        extensions: [".d.ts", ".js", ".json", ".jsx", ".ts", ".tsx"],
-      },
-      typescript: {
-        //
-      },
-    },
+    // add your own!
   },
 };
